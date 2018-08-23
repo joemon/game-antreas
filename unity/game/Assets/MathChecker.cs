85befb58e384959136b42d21869d5b797d5d15af
antreas-kontopoulos <35497274+antreas-kontopoulos@users.noreply.github.com>
2018-08-23 05:05:33 +0100

    Implementation of 5 different levels inside the game

---
+++ unity/game/Assets/MathChecker.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MathChecker : MonoBehaviour {
+    string password;
+
+    string[] level1Εquations = { "8 + 5 * 2 = ?", "15 + 16 / 2 = ?", "12 * 4 - 3 = ?", "11 * 12 - 20 = ?" };
+    string[] level1Passwords = { "18", "23", "45", "112" };
+
+    string[] level2Εquations = { "8 * (8 - 6 / 3) = ?", "10 * 3 / (4 + 1 * 2) = ?", "(144 / (3 * 6 - 6)", "9 * (48 / 2 / 2)" };
+    string[] level2Passwords = { "48", "5", "12", "108" };
+
+    string[] level3Εquations = { "(8 * 7 - 1) / (1 + (15 * 4 / 6)) = ?", "(5^2 + 5) / ( 3 * ( 5 * 4 / 10 / 2 )) = ?", "(9 * 7) * ( 2^3 / (8 - 2 * 2)) = ?" };
+    string[] level3Passwords = { "5", "10", "126" };
+
+    public Text message;
+    public Text equation;
+    public InputField inputField;
+    
+
+    // Use this for initialization
+    void Start () {
+        inputField.Select();
+        inputField.ActivateInputField();
+        inputField.onEndEdit.AddListener(delegate { ValueChangeCheck(); });
+        SelectEquation();
+    }
+
+
+    private void ValueChangeCheck()
+    {
+        if (inputField.text == "menu")
+        {
+            // ShowMainMenu();
+        }
+        else if (inputField.text == "quit" || inputField.text == "close" || inputField.text == "exit")
+        {
+            inputField.text = "";
+            equation.text = "";
+            message.text = "If you want to exit just close the tab.";
+            Application.Quit();
+        }
+        else
+        {
+            CheckPassword(inputField.text);
+        }
+    }
+
+    private void SelectEquation()
+    {
+        int index = -1;
+        if (SceneManager.GetActiveScene().buildIndex < 2)
+        {
+            index = Random.Range(0, level1Passwords.Length);
+            password = level1Passwords[index];
+            equation.text = level1Εquations[index];
+        }
+        else if (SceneManager.GetActiveScene().buildIndex < 4)
+        {
+            index = Random.Range(0, level2Passwords.Length);
+            password = level2Passwords[index];
+            equation.text = level2Εquations[index];
+        }
+        else
+        {
+            index = Random.Range(0, level3Passwords.Length);
+            password = level3Passwords[index];
+            equation.text = level3Εquations[index];
+        }
+
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            inputField.text = "";
+            message.text = "Wrong answer. Please try again.";
+            Start();
+        }
+    }
+
+}
---
+++ unity/game/Assets/Rocket.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip winLevel;
+    [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem mainEngineParticle;
+    [SerializeField] ParticleSystem winLevelParticle;
+    [SerializeField] ParticleSystem deathParticle;
+
+    public GameObject completeLevelUI;
+   
+
+    enum State { Alive, Dying, WinStage}
+    State state = State.Alive;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (state == State.Alive){
+            Thrust();
+            Rotate();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive){ return; }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                state = State.WinStage;
+                audioSource.Stop();
+                audioSource.PlayOneShot(winLevel);
+                winLevelParticle.Play();
+                Invoke( "CompleteLevel", 1f);
+               // Invoke("LoadNextScene", 1.5f);
+                break;
+            default:
+                state = State.Dying;
+                audioSource.Stop();
+                audioSource.PlayOneShot(death);
+                deathParticle.Play();
+                mainEngineParticle.Stop();
+                Invoke("LoadNextScene", 1f);
+                break;
+        }
+    }
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+       
+        
+    }
+
+    private void LoadNextScene()
+    {
+        if (state == State.WinStage) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else if (state == State.Dying){    
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);   // I add * Time.deltaTime
+            if (!audioSource.isPlaying){
+                audioSource.PlayOneShot(mainEngine);
+            }
+            mainEngineParticle.Play();
+        }
+        else{
+            audioSource.Stop();
+            mainEngineParticle.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false;
+    }
+
+}
